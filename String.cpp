7bf3019c8a817d6b31ebc27ddf6e5db1fbcc0375
bailin048 <foreb@qq.com>
2021-03-26 22:17:59 +0800

    2021_3_26：深浅拷贝

--- String.cpp
+++ String.cpp
@@ -12,15 +12,12 @@ public:
 	String_cnt(const char* str = "") :m_cnt(0) {
 		m_data = new char[strlen(str) + 1];
 		strcpy(m_data, str);
-		Increase();
 	}
 	String_cnt(const String_cnt& s) {
-		delete[]m_data;
-		m_data = new char[strlen(s.m_data) + 1];
-		Increase();
+		m_data = s.m_data;
 	}
 	String_cnt& operator=(const String_cnt& s) {
-		if (this != &s) 
+		if (this != &s) //浅拷贝
 			m_data = s.m_data;
 		return *this;
 	}
@@ -45,43 +42,50 @@ class String {
 	friend ostream& operator<<(ostream& out, const String& s);
 public:
 	explicit String(const char* str = "") :pn(new String_cnt(str)) {
-		pn->Increase();
+		pn->Increase();//构造函数，串计数器+1
 	}
-	String(const String& s) :pn(s.pn) { pn->Increase(); }
+	String(const String& s) :pn(s.pn) { pn->Increase(); }//拷贝构造，浅拷贝，新串计数器+1
 	String& operator=(const String& s) {
 		if (this != &s) {
-			pn->Decrease();
-			pn = s.pn;
-			pn->Increase();
+			pn->Decrease();//原串计数器-1
+			pn = s.pn;//浅拷贝指向新串
+			pn->Increase();//新串计数器+1
 		}
 		return *this;
 	}
-	~String() { pn->Decrease(); }
+	~String() { pn->Decrease(); }//析构函数，只需将串的计数器减1
 	void ToUpper() {
-		String_cnt* tmp = new String_cnt(pn->m_data);
-		pn->Decrease();
-		pn = tmp;
-		pn->Increase();
-		char* str = pn->m_data;
-		while (*str) {
+		DepthCopy();//写时拷贝
+		char* str = pn->m_data;//拿到深拷贝的串地址
+		while (*str) {//小写转大写
 			if (*str >= 'a' && *str <= 'z')
 				*str -= 32;
 			str++;
 		}
 	}
 	void ToLower() {
-		String_cnt* tmp = new String_cnt(pn->m_data);
-		pn->Decrease();
-		pn = tmp;
-		pn->Increase();
-		char* str = pn->m_data;
-		while (*str) {
+		DepthCopy();//写时拷贝
+		char* str = pn->m_data;//拿到深拷贝的串地址
+		while (*str) {//大写转小写
 			if (*str >= 'A' && *str <= 'Z')
 				*str += 32;
 			str++;
 		}
+	}	
+	int Size() {
+		int length = 0;
+		char* p = pn->m_data;
+		while (*p)
+			++length, ++p;
+		return length;
 	}
 private:
+	void DepthCopy() {//深拷贝——仅供写实拷贝使用，私有方法
+		String_cnt* tmp = new String_cnt(pn->m_data);
+		pn->Decrease();//先将原串的计数器-1
+		pn = tmp;//获得新串的地址
+		pn->Increase();//新串计数器+1
+	}
 	String_cnt* pn;
 };
 
@@ -92,16 +96,9 @@ ostream& operator<<(ostream& out, const String& s) {
 int main()
 {
 	String s1("adutsdutfsogfsif");
-	String s2(s1);
-	cout << s1 << endl;
-	String s3;
-	s3 = s1;
-	s3.ToUpper();
-	cout << s1 << endl;
-	cout << s2 << endl;
-	cout << s3 << endl;
-	s3.ToLower();
-	cout << s1 << endl;
-	cout << s3 << endl;
+	String s2("31231");
+	cout << s1.Size() << endl;
+	cout << s2.Size() << endl;
+
 	return 0;
 }
